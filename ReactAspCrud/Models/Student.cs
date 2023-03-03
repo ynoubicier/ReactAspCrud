e9922ee2a32c279d3123ae28572687de883b8b99
ynoubicier <yvanyanova@outlook.fr>
2023-03-03 04:08:26 +0100

    Ajoutez des fichiers projet.

---
+++ ReactAspCrud/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string stname { get; set; }
+        public string course { get; set; }
+    }
+}
---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=studentbase;Integrated Security=True;Pooling=False");
+        }
+    }
+}
